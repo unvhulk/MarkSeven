19fdd6d284ae458598b1b1f66d6933f1fbf16c0d
unvhulk <unvhulk@gmail.com>
2021-12-09 16:42:56 +0530

    Initial comit

---
+++ index.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error) {
+  console.log(`error occured: ${error}`);
+}
+
+function getTranslationURL(text) {
+  return `${serverURL}?text=${text}`;
+}
+
+function clickEventHandler() {
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((resp) => resp.json())
+    .then((translated) => {
+      console.log(translated);
+      let txtTranslated = translated.contents.translated;
+      txtOutput.innerText = txtTranslated;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
